c7d8ed0a8c14f25caa949cf108c1fb99f623e1f7
sdchjjj <sdchjjj@gmail.com>
2025-12-11 16:01:11 +0900

    [서동철]스테이터스 이벤트 추가 : 체력, 온도 값 변경

--- Source/FrozenBreak/Public/GameSystem/Events/StatusEvents.h
+++ Source/FrozenBreak/Public/GameSystem/Events/StatusEvents.h
@@ -5,9 +5,19 @@
 #include "CoreMinimal.h"
 #include "StatusEvents.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealthPointChanged, float, InCurrentHealth);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnTemperaturePointChanged, float, InCurrentTemperature);
+
 USTRUCT(BlueprintType)
 struct FStatusEvents
 {
 	GENERATED_BODY()
 
+	// 플레이어 체력
+	UPROPERTY(BlueprintAssignable)
+	FOnHealthPointChanged OnHealthPointChanged;
+
+	// 플레이어 체온
+	UPROPERTY(BlueprintAssignable)
+	FOnTemperaturePointChanged OnTemperaturePointChanged;
 };
