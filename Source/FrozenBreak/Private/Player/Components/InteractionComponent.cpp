58a2a49b213e609b45df317b53f806950fd1307d
LEI-WULONG <dngustn3@gmail.com>
2025-12-11 15:14:18 +0900

    [우현수] InteractionComponent 생성 및 초기작업, 트레이스 채널 생성

---
+++ Source/FrozenBreak/Private/Player/Components/InteractionComponent.cpp
@@ -0,0 +1,85 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/Components/InteractionComponent.h"
+#include "Camera/CameraComponent.h"
+
+// Sets default values for this component's properties
+UInteractionComponent::UInteractionComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.TickInterval = 0.05f; // 틱 도는 빈도를 1초에 20번 정도로 제한
+
+}
+
+
+// Called when the game starts
+void UInteractionComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ComponentOwner = GetOwner();
+	Camera = ComponentOwner->FindComponentByClass<UCameraComponent>();
+	if (ComponentOwner && Camera)
+	{
+		// 이 컴포넌트의 주인은 라인트레이스 히트에서 제외됨
+		QueryParams.AddIgnoredActor(ComponentOwner);
+	}
+	else
+	{
+		// 이런 일이 있을 수가 있나
+		return;
+	}
+
+}
+
+
+// Called every frame
+void UInteractionComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// 카메라의 ForwardVector 기준으로 라인 트레이싱 (화면 중앙)
+	const FVector CameraLocation = Camera->GetComponentLocation();
+	const FVector CameraForward = Camera->GetForwardVector();
+	const FVector TargetLocation = CameraLocation + (CameraForward * InteractionDistance);
+
+	const bool bHit = GetWorld()->LineTraceSingleByChannel
+	(
+		InteractionHitResult,		// 라인에 블록된 액터
+		CameraLocation,			
+		TargetLocation,
+		InteractableActorChannel,	// InteractableActorChannel이 Block으로 되어있는 액터만
+		QueryParams					// 제외할 것 (컴포넌트 주인)
+	);
+
+#if WITH_EDITOR
+	DrawDebugLine(GetWorld(), CameraLocation, TargetLocation, FColor::Green, false, -1.f, 0, 0.25f);
+#endif
+
+	AActor* HitActor = InteractionHitResult.GetActor();
+
+	if (bHit)
+	{
+		if (!bIsInteracting)
+		{
+			UE_LOG(LogTemp, Log, TEXT("Hit Actor : %s"), *InteractionHitResult.GetActor()->GetName());
+
+		}
+		LastInteractionActor = HitActor;
+		bIsInteracting = true;
+	}
+	else
+	{
+		if (bIsInteracting)
+		{
+			UE_LOG(LogTemp, Log, TEXT("Hit Actor End"));
+		}
+
+
+		bIsInteracting = false;
+	}
+}
+
---
+++ Source/FrozenBreak/Public/Player/Components/InteractionComponent.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "InteractionComponent.generated.h"
+
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class FROZENBREAK_API UInteractionComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UInteractionComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+	// 라인 충돌지점에 뭐가 충돌했는지 알려줌
+	FHitResult InteractionHitResult;
+
+	// 라인을 쏠 때 충돌 제외할 것 (플레이어)
+	FCollisionQueryParams QueryParams;
+
+	// 트레이스 채널, 이 트레이스 채널이 블록된 액터만 상호작용 가능 (일반 액터의 트레이스 방지)
+	const ECollisionChannel InteractableActorChannel = ECC_GameTraceChannel1;
+
+	const AActor* ComponentOwner = nullptr;
+
+	// 마지막으로 트레이싱 된 액터를 저장
+	AActor* LastInteractionActor = nullptr;
+
+	const class UCameraComponent* Camera = nullptr;
+
+protected:
+	// 라인의 길이
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float InteractionDistance = 200.f;
+
+	// 한번만 실행되게 만들기 위한 bool
+	bool bIsInteracting = false;
+};
