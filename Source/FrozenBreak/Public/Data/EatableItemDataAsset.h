9d2f9e5d4140f5bc6ac7ed114a70b2dc54453e7b
LEI-WULONG <dngustn3@gmail.com>
2025-12-15 14:38:56 +0900

    ItemDataAsset 통합

---
+++ Source/FrozenBreak/Public/Data/EatableItemDataAsset.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Data/ItemDataAssetBase.h"
+#include "EatableItemDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FROZENBREAK_API UEatableItemDataAsset : public UItemDataAssetBase
+{
+	GENERATED_BODY()
+	
+public:
+	
+};
