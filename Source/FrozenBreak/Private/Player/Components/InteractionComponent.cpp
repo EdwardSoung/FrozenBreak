6eb940e13d67b827bb6498f42a77f4c5e7816ddc
LEI-WULONG <dngustn3@gmail.com>
2025-12-16 14:45:02 +0900

    [우현수] Interaction Component 주석 추가

--- Source/FrozenBreak/Private/Player/Components/InteractionComponent.cpp
+++ Source/FrozenBreak/Private/Player/Components/InteractionComponent.cpp
@@ -25,11 +25,12 @@ void UInteractionComponent::BeginPlay()
 	Camera = ComponentOwner->FindComponentByClass<UCameraComponent>();
 	if (ComponentOwner && Camera)
 	{
-
+		
 	}
 	else
 	{
 		// 이런 일이 있을 수가 있나
+		UE_LOG(LogTemp, Warning, TEXT("플레이어 혹은 카메라 컴포넌트가 없습니다"));
 		return;
 	}
 
@@ -60,42 +61,56 @@ void UInteractionComponent::TickComponent(float DeltaTime, ELevelTick TickType, 
 
 	CurrentInteractionActor = InteractionHitResult.GetActor();
 
+	// 뭔가 라인에 맞았다
 	if (bHit)
 	{
+		// 지금 상호작용 하고있지 않다면
 		if (!bIsInteracting)
 		{
+			// 바라보고 있는 액터가 있다
 			if (CurrentInteractionActor)
 			{
+				// 바라보고 있는 액터에게 "너 지금 바라봐지고 있어" 라고 알림
 				IInteractable::Execute_OnSelect(CurrentInteractionActor, true);
 				UE_LOG(LogTemp, Log, TEXT("Hit Actor : %s"), *InteractionHitResult.GetActor()->GetName());
 				bIsInteracting = true;
 			}
 		}
+		// 바라보고 있는 액터와 마지막으로 바라본 액터가 다르다면
 		else if (CurrentInteractionActor != LastInteractionActor)
 		{
+			// 마지막으로 바라봤던 액터에게는
 			if (LastInteractionActor)
 			{
+				// "너 이제 안 보고 있어" 라고 알림
 				IInteractable::Execute_OnSelect(LastInteractionActor, false);
 				UE_LOG(LogTemp, Log, TEXT("Hit Actor End (Switch): %s"), *LastInteractionActor->GetName());
 			}
+			// 현재 새롭게 바라보고 있는 액터에게는
 			if (CurrentInteractionActor)
 			{
+				// "너 지금 바라봐지고 있어" 알림
 				IInteractable::Execute_OnSelect(CurrentInteractionActor, true);
 				UE_LOG(LogTemp, Log, TEXT("Hit Actor : %s"), *CurrentInteractionActor->GetName());
 			}
 		}
 		LastInteractionActor = CurrentInteractionActor;
-
 	}
+	// 라인에 맞은게 없다
 	else
 	{
+		// 상호작용 하고 있었다면
 		if (bIsInteracting)
 		{
+			// 마지막으로 바라봤었던 (진짜 방금 전까지 보고있었던) 액터에게
 			if (LastInteractionActor)
 			{
+				// "너 이제 안 보고 있어" 알림
 				IInteractable::Execute_OnSelect(LastInteractionActor, false);
 				UE_LOG(LogTemp, Log, TEXT("Hit Actor End"));
 			}
+
+			// 초기화
 			CurrentInteractionActor = nullptr;
 			LastInteractionActor = nullptr;
 			bIsInteracting = false;
@@ -103,16 +118,16 @@ void UInteractionComponent::TickComponent(float DeltaTime, ELevelTick TickType, 
 	}
 }
 
-void UInteractionComponent::DoAction_Implementation()
+void UInteractionComponent::DoAction_Implementation() // 플레이어가 상호작용 키를 눌렀을 때
 {
-	bool bIsTryAction = false;
-
 	if (bIsInteracting)		// 상호작용 가능한 액터를 바라보고 있을 때만
 	{
-		if (CurrentInteractionActor)
+		if (CurrentInteractionActor) // 바라보고 있는 액터에게
 		{
+			// "너가 할 수 있는거 하셈" 알림
 			IInteractable::Execute_DoAction(LastInteractionActor);
 
+			// 초기화
 			CurrentInteractionActor = nullptr;
 			LastInteractionActor = nullptr;
 			bIsInteracting = false;
