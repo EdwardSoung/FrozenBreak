942dab31bcd887c0ad6f15a3a2d772a67413f374
sdchjjj <sdchjjj@gmail.com>
2025-12-16 13:14:26 +0900

    [서동철] 적 체력바 생성

---
+++ Source/FrozenBreak/Private/UI/Enemy/EnemyHealthBarWidget.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/Enemy/EnemyHealthBarWidget.h"
+#include "Components/ProgressBar.h"
+
+void UEnemyHealthBarWidget::SetHealthProgress(float InCurrentHealth)
+{
+	EnemyHealthBar->SetPercent(InCurrentHealth);
+}
