efd03611e18ca53cf19e80e285ff394f9d6cadca
LEI-WULONG <dngustn3@gmail.com>
2025-12-16 10:06:21 +0900

    [우현수] 아이템 데이터 에셋 개편 및 변경, 에디터에 추가

--- Source/FrozenBreak/Public/Data/Enums.h
+++ Source/FrozenBreak/Public/Data/Enums.h
@@ -21,13 +21,13 @@ enum class EItemType : uint8
 	Timber,		// 목재
 	Stone,		// 돌멩이
 	Vine,		// 덩쿨
+	Lether,		// 동물 가죽
 	Axe,		// 도끼
 	Pickaxe,	// 곡괭이
 	Knife,		// 칼
 	RawMeat,	// 생고기
 	CookedMeat,	// 구운 고기
 	Fruit,		// 과일?
-	Lether,		// 동물 가죽
 	Jaket		// 자켓
 };
 
@@ -37,9 +37,9 @@ enum class EPropType : uint8
 	None,
 	Tree,				// 나무
 	Rock,				// 돌덩이 (길을 막는 돌덩이랑 차별점을 둘지 고민)
-	CraftingTable,		// 제작대
 	Bed,				// 침대
-	Campfire			// 모닥불?? prop에 있어도 되나
+	CraftingTable,		// 제작대	<- 애매함
+	Campfire			// 모닥불	<- 동일
 
 };
 
