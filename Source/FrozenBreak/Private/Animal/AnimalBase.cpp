081d6f27c617440f18707102909f760b67e4e6d5
sdchjjj <sdchjjj@gmail.com>
2025-12-23 15:13:35 +0900

    [서동철] 동물 - 사슴 기본 구현 : 기초 AI(돌아다니기, 도망), 사망, 아이템 드랍 처리

---
+++ Source/FrozenBreak/Private/Animal/AnimalBase.cpp
@@ -0,0 +1,115 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Animal/AnimalBase.h"
+#include "Components/WidgetComponent.h"
+#include "UI/Enemy/EnemyHealthBarWidget.h"
+#include "GameSystem/ItemFactorySubSystem.h"
+
+// Sets default values
+AAnimalBase::AAnimalBase()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	HealthBarWidgetComponent = CreateDefaultSubobject<UWidgetComponent>(TEXT("HealthBarWidget"));
+	HealthBarWidgetComponent->SetupAttachment(RootComponent);
+	HealthBarWidgetComponent->SetRelativeLocation(FVector(0, 0, 100));
+	HealthBarWidgetComponent->SetWidgetSpace(EWidgetSpace::Screen);
+	HealthBarWidgetComponent->SetVisibility(false);
+}
+
+// Called when the game starts or when spawned
+void AAnimalBase::BeginPlay()
+{
+	Super::BeginPlay();
+	CurrentHealth = MaxHealth;
+
+	HealthBarWidgetComponent->InitWidget(); // 위젯 생성 보장(중요)
+	HealthBar = Cast<UEnemyHealthBarWidget>(HealthBarWidgetComponent->GetUserWidgetObject());
+
+	if (HealthBar)
+	{
+		HealthBar->SetHealthProgress(CurrentHealth / MaxHealth);
+	}
+
+	OnTakeAnyDamage.AddDynamic(this, &AAnimalBase::OnTakeDamage);
+}
+
+// Called to bind functionality to input
+void AAnimalBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+void AAnimalBase::OnTakeDamage(
+	AActor* DamagedActor,
+	float Damage,
+	const UDamageType* DamageType, 
+	AController* InstigatedBy, 
+	AActor* DamageCauser)
+{
+	CurrentHealth = FMath::Max(CurrentHealth - Damage, DefaultMinimum);
+	if (HealthBar)
+	{
+		HealthBar->SetHealthProgress(CurrentHealth / MaxHealth);
+		StartHealthBarTimer();
+		if (CurrentHealth <= DefaultMinimum)
+		{
+			PlayDead();
+		}
+	}
+}
+
+
+void AAnimalBase::StartHealthBarTimer()
+{
+	HealthBarWidgetComponent->SetVisibility(true);
+
+	GetWorldTimerManager().SetTimer(
+		HealthBarTimerHandle,
+		[this]() {
+			HealthBarWidgetComponent->SetVisibility(false);
+		},
+		HealthBarVisibleTime,
+		false
+		);
+}
+
+void AAnimalBase::PlayDead()
+{
+	IDamageable::Execute_OnDead(this);
+	HealthBarWidgetComponent->SetVisibility(false);
+
+	if (AnimalDeadAnimation)
+	{
+		if (USkeletalMeshComponent* MeshComp = GetMesh())
+		{
+			if (UAnimInstance* Anim = MeshComp->GetAnimInstance())
+			{
+				Anim->Montage_Play(AnimalDeadAnimation);
+			}
+		}
+	}
+
+	GetWorldTimerManager().SetTimer(
+		DeadTimerHandle,
+		this,
+		&AAnimalBase::DestroyAnimal,
+		2.5f,
+		false
+	);
+}
+
+void AAnimalBase::DestroyAnimal()
+{
+	if (UItemFactorySubSystem* ItemFactory = UItemFactorySubSystem::Get(this))
+	{
+		ItemFactory->Spawn(EItemType::Lether, GetActorLocation() + FVector(10, 10, 50), LeatherDropCount);
+		for (int i = 0; i < RawMeatDropCount; i++)
+		{
+			ItemFactory->Spawn(EItemType::RawMeat, GetActorLocation() + FVector(-10, -10, 50), DefaultMinimum);
+		}
+	}
+	Destroy();
+}
---
+++ Source/FrozenBreak/Public/Animal/AnimalBase.h
@@ -0,0 +1,72 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Interface/Damageable.h"
+#include "AnimalBase.generated.h"
+
+UCLASS()
+class FROZENBREAK_API AAnimalBase : public ACharacter, public IDamageable
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AAnimalBase();
+
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION()
+	void OnTakeDamage(
+		AActor* DamagedActor,
+		float Damage, 
+		const UDamageType* DamageType,
+		AController* InstigatedBy,
+		AActor* DamageCauser);
+
+private:
+	void StartHealthBarTimer();
+
+	void PlayDead();
+
+	UFUNCTION()
+	void DestroyAnimal();
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float MaxHealth = 100.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float CurrentHealth = 0.0f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<class UWidgetComponent> HealthBarWidgetComponent = nullptr;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<class UEnemyHealthBarWidget> HealthBar = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<class UAnimMontage> AnimalDeadAnimation = nullptr;
+
+	float DefaultMinimum = 0.0f;
+
+	UPROPERTY()
+	int32 RawMeatDropCount = 2;
+
+	UPROPERTY()
+	int32 LeatherDropCount = 1;
+
+private:
+	FTimerHandle HealthBarTimerHandle;
+	FTimerHandle DeadTimerHandle;
+	float HealthBarVisibleTime = 5.0f;
+};
