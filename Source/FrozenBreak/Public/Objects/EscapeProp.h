a99ccc4f4861a0be0be88e98a650ebd9d1d57837
sdchjjj <sdchjjj@gmail.com>
2025-12-30 14:00:12 +0900

    Revert "[서동철] 변수 접근자 변경(빌드 오류 수정)"
    This reverts commit caf0944ba65fdd7f1da6222a7843e4eab712f7d2.

--- Source/FrozenBreak/Public/Objects/EscapeProp.h
+++ Source/FrozenBreak/Public/Objects/EscapeProp.h
@@ -40,9 +40,6 @@ protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Prop|Widget")
 	TObjectPtr<class UWidgetComponent> DurabilityWidget = nullptr;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Prop|Data")
-	float FatigueCostPerWork = -50.0f;
-
 	const ECollisionChannel InteractableActorChannel = ECollisionChannel::ECC_GameTraceChannel1;
 public:
 	void RockAction();
@@ -53,6 +50,9 @@ public:
 	void OnSelect_Implementation(bool bIsStart) override;
 
 private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Prop|Data")
+	float FatigueCostPerWork = -50.0f;
+
 	float ToolAtkPower = 0.f;
 
 	float MaxDurability = 0.0f;
